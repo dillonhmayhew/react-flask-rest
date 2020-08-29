9b823f24e13692af7a1477c4b1f31fd73b44eb6f
Dillon Mayhew <dillonhmayhew@gmail.com>
2020-08-29 01:26:49 -0600

    converted App to functional component

--- src/App.js
+++ src/App.js
@@ -16,24 +16,22 @@ import {
 import './App.css';
 
 
-class App extends React.Component {
-	render() {
-		return (
-			<>
-				<Switch>
-					<Route path="/400" component={BadRequest} />
-					<Route path="/401" component={Unauthorized} />
-					<Route path="/404" component={NotFound} />
-					<Route path="/405" component={MethodNotAllowed} />
-					<>
-						<Header />
-						<Route path="/tasks/:taskID" component={Task} />
-						<Route path="/" />
-					</>
-				</Switch>
-			</>
-		);
-	}
+const App = () => {
+	return (
+		<>
+			<Switch>
+				<Route path="/400" component={BadRequest} />
+				<Route path="/401" component={Unauthorized} />
+				<Route path="/404" component={NotFound} />
+				<Route path="/405" component={MethodNotAllowed} />
+				<>
+					<Header />
+					<Route path="/tasks/:taskID" component={Task} />
+					<Route path="/" />
+				</>
+			</Switch>
+		</>
+	);
 }
 
 export default App;
