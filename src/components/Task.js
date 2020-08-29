ae69db5e9ca9c505f574a437c0e04146ade2be68
Dillon Mayhew <dillonhmayhew@gmail.com>
2020-08-29 00:23:09 -0600

    converted Task back to functional component

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,86 +1,83 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactTable from "react-table";
 import 'react-table/react-table.css';
 import { Redirect } from 'react-router-dom';
 import { TaskForm } from './forms'
 
+const Task = ({match}) => {
+    const {params: {taskID}} = match;
+  
+    const [task, setTask] = useState([{}]);
+    const [errorStatus, setErrorStatus] = useState(0);
 
-class Task extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            task: [{}],
-            taskID: props.match.params.taskID,
-            errorStatus: null
+    const columns = [
+        {
+            Header: "Title",
+            accessor: "title",
+            width: 250,
+            sortable: false,
+            style:{
+                textAlign: "center"
+            },
+        },
+        {
+            Header: "Done",
+            accessor: "done",
+            width: 100,
+            sortable: false,
+            resizable: false,
+            style:{
+                textAlign: "center"
+            },
+        },
+        {
+            Header: "Description",
+            accessor: "description",
+            sortable: false,
+            style:{
+                textAlign: "center"
+            },
         }
-    }
-
-    componentDidMount() {
-        fetch(`/api/tasks/${this.state.taskID}`, {
+    ]
+    
+    useEffect(() => {
+        fetch(`/api/tasks/${taskID}`, {
             method: 'GET'
         })
         .then(res => res.json())
         .then(data => {
             if (data.error) {
-                this.setState({errorStatus: data.status});
+                setErrorStatus(data.status);
             } else {
-                data.task.done = String(data.task.done);
-                this.setState({task: [data.task]});
+                setTask([{
+                    title: data.task.title,
+                    done: String(data.task.done),
+                    description: data.task.description
+                }]);
             }
         });
-    }
-    render() {
-        const columns = [
-            {
-                Header: "Title",
-                accessor: "title",
-                width: 250,
-                sortable: false,
-                style:{
-                    textAlign: "center"
-                },
-            },
-            {
-                Header: "Done",
-                accessor: "done",
-                width: 100,
-                sortable: false,
-                resizable: false,
-                style:{
-                    textAlign: "center"
-                },
-            },
-            {
-                Header: "Description",
-                accessor: "description",
-                sortable: false,
-                style:{
-                    textAlign: "center"
-                },
-            }
-        ]
+    }, []);
 
-        if (this.state.errorStatus) return <Redirect to={`/${this.state.errorStatus}`} />;
-    
-        return (
-            <>
-                <TaskForm 
-                    taskID={this.state.taskID}
-                    title={this.state.task[0].title} 
-                    done={this.state.task[0].done} 
-                    description={this.state.task[0].description} 
-                    bText='Update'
-                    method='PUT'
-                />
-                <ReactTable 
-                    columns={columns} 
-                    data={this.state.task}
-                    showPagination={false}
-                    defaultPageSize={this.state.task.length}
-                />
-            </>
-        );
-    }
+    if (errorStatus) return <Redirect to={`/${errorStatus}`} />;
+  
+    return (
+        <>
+            <TaskForm 
+                taskID={taskID}
+                title={task[0].title} 
+                done={task[0].done} 
+                description={task[0].description} 
+                bText='Update'
+                method='PUT'
+            />
+            <ReactTable 
+                columns={columns} 
+                data={task}
+                showPagination={false}
+                defaultPageSize={task.length}
+            />
+        </>
+    );
 }
 
 export default Task;
