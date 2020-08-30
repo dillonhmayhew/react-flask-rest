2227ca583eebcb9f1f6fb15f74d760481624d9ee
Dillon Mayhew <dillonhmayhew@gmail.com>
2020-08-29 18:31:55 -0600

    TaskForm displays on a Modal

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,135 +2,147 @@ import React, { useState, useEffect } from 'react';
 import ReactTable from "react-table";
 import 'react-table/react-table.css';
 import { Redirect } from 'react-router-dom';
-import { TaskForm } from './forms'
 import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal'
+import { TaskForm } from './forms';
 
 const Task = (props) => {
-    const {params: {taskID}} = props.match;
-  
-    const [task, setTask] = useState([{}]);
-    const [errorStatus, setErrorStatus] = useState(0);
-    const [deleted, setDeleted] = useState(false);
-    const [update, setUpdate] = useState(false);
-
-    const columns = [
-        {
-            Header: "Title",
-            accessor: "title",
-            width: 250,
-            sortable: false,
-            style:{
-                textAlign: "center"
-            },
-        },
-        {
-            Header: "Done",
-            accessor: "done",
-            width: 100,
-            sortable: false,
-            resizable: false,
-            style:{
-                textAlign: "center"
-            },
-        },
-        {
-            Header: "Description",
-            accessor: "description",
-            sortable: false,
-            resizable: false,
-            style:{
-                textAlign: "center"
-            },
-        },
-        {
-          Header: "Actions",
-          filterable: false,
-          sortable: false,
-          resizable: false,
-          style:{
-            textAlign: "center"
-            },
-          Cell: () =>{
-            return(
-                <>
-                <Button variant='outline-dark' onClick={(e)=> {
-                    alert("update button"); // open update modal with form
-                }}
-                >Edit</Button>
-                <Button variant='outline-light' onClick={()=> {
-                        handleDelete()
-                    }}
-                >Delete</Button>
-                </>
-          )},
-          width: 150,
-          maxWidth: 150,
-          minWidth: 150,
-        }
-    ]
-    
-    // GET data from API
-    useEffect(() => {
-        fetch(`/api/tasks/${taskID}`, {
-            method: 'GET'
-        })
-        .then(res => res.json())
-        .then(data => {
-            if (data.error) {
-                setErrorStatus(data.status);
-            } else {
-                setTask([{
-                    title: data.task.title,
-                    done: String(data.task.done),
-                    description: data.task.description
-                }]);
-                setUpdate(false);
-            }
-        });
-    }, [taskID, update]);
-
-    const handleDelete = (e) => {
-        // e.preventDefault();
-        fetch(`/api/tasks/${taskID}`, {
-            method: 'DELETE'
-        })
-        .then(res => res.json())
-        .then(data => {
-            if (data.error) {
-                setErrorStatus(data.status);
-            } else {
-                setDeleted(data.result);
-            }
-        });
-    }
-
-    const handleForm = () => {
-        setUpdate(true);
-    }
-
-    if (errorStatus) return <Redirect to={`/${errorStatus}`} />;
-    if (deleted) return <Redirect to='/tasks' />;
-  
-    return (
-        <>
-            <TaskForm 
-                taskID={taskID}
-                title={task[0].title} 
-                done={task[0].done} 
-                description={task[0].description} 
-                bText='Update'
-                method='PUT'
-                handler={handleForm}
-            />
-            <ReactTable 
-                columns={columns} 
-                data={task}
-                showPagination={false}
-                defaultPageSize={task.length}
-            />
-        </>
-    );
+	const {params: {taskID}} = props.match;
+	
+	const [task, setTask] = useState([{}]);
+	const [errorStatus, setErrorStatus] = useState(0);
+	const [deleted, setDeleted] = useState(false);
+	const [updated, setUpdated] = useState(false);
+	const [showModal, setShowModal] = useState(false);
+	
+	const handleForm = () => setUpdated(true);
+	const childErrorStatus = (err) => setErrorStatus(err)
+	const openEditModal = () => setShowModal(true);
+	const closeEditModal = () => setShowModal(false);
+	
+	const columns = [
+		{
+			Header: "Title",
+			accessor: "title",
+			width: 250,
+			sortable: false,
+			style:{
+				textAlign: "center"
+			},
+		},
+		{
+			Header: "Done",
+			accessor: "done",
+			width: 100,
+			sortable: false,
+			resizable: false,
+			style:{
+				textAlign: "center"
+			},
+		},
+		{
+			Header: "Description",
+			accessor: "description",
+			sortable: false,
+			resizable: false,
+			style:{
+				textAlign: "center"
+			},
+		},
+		{
+			Header: "Actions",
+			filterable: false,
+			sortable: false,
+			resizable: false,
+			style:{
+				textAlign: "center"
+		},
+		Cell: () => {
+			return (
+				<>
+				<Button variant='outline-dark' onClick={() => {
+					openEditModal();
+				}}
+				>Edit</Button>
+				<Button variant='outline-light' onClick={(e)=> {
+					alert("DELETE");    // deleteTask(e);
+				}}
+				>Delete</Button>
+				</>
+				)},
+				width: 150,
+				maxWidth: 150,
+				minWidth: 150,
+				padding: 10
+			}
+		]
+		
+	const formProps = {
+		taskID: taskID,
+		title: task[0].title, 
+		done: task[0].done, 
+		description: task[0].description, 
+		bText: 'Update',
+		method: 'PUT',
+		handlerForm: handleForm,
+		formError: childErrorStatus
+	}
+	
+	const modalProps = {
+		showModal: showModal,
+		handlerModal: closeEditModal
+	}
+	
+	// GET data from API
+	useEffect(() => {
+		fetch(`/api/tasks/${taskID}`, {
+			method: 'GET'
+		})
+		.then(res => res.json())
+		.then(data => {
+			if (data.error) {
+				setErrorStatus(data.status);
+			} else {
+				setTask([{
+					title: data.task.title,
+					done: String(data.task.done),
+					description: data.task.description
+				}]);
+				setUpdated(false);
+				setDeleted(false);
+			}
+		});
+	}, [taskID, updated, deleted]);
+	
+	// DELETE Task
+	const deleteTask = (e) => {
+		e.preventDefault();
+		fetch(`/api/tasks/${taskID}`, {
+			method: 'DELETE'
+		})
+		.then(res => res.json())
+		.then(data => {
+			if (data.error) {
+				setErrorStatus(data.status);
+			} else {
+				setDeleted(data.result);
+			}
+		});
+	}
+	
+	if (errorStatus) return <Redirect to={`/${errorStatus}`} />;
+	if (deleted) return <Redirect to='/tasks' />;
+			
+	return (
+		<>
+			{showModal && <TaskForm {...modalProps} {...formProps} />}
+			<ReactTable 
+			columns={columns} 
+			data={task}
+			showPagination={false}
+			defaultPageSize={task.length}
+			/>
+		</>
+	);
 }
 
 export default Task;
--- src/components/forms/TaskForm.js
+++ src/components/forms/TaskForm.js
@@ -1,19 +1,29 @@
-import React, { useState, useEffect } from 'react';
-// import { Redirect } from 'react-router-dom'
-import Form from 'react-bootstrap/Form';
+import React, { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
-// import Alert from 'react-bootstrap/Alert';
 
-const TaskForm = (props) => {
+
+const TaskFormModal = (props) => {
+	// Modal
+	const [show, setShow] = useState(props.showModal);
+
+	// Form
     const [title, setTitle] = useState(props.title);
     const [done, setDone] = useState(props.done);
     const [description, setDescription] = useState(props.description);
-    const [errorStatus, setErrorStatus] = useState(null);
-    const [success, setSuccess] = useState(false);
+	
+	// Modal
+	const handleClose = () => {
+		setShow(false);
+		// allow animation to happen before Task Component hides it
+		setTimeout(() => props.handlerModal(), 1000);
+	}
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+	// Form
+	const handleSubmit = (e) => {
+		e.preventDefault();
         fetch(`/api/tasks/${props.taskID}`, {
             method: props.method,
             headers: {'Content-Type': 'application/json'},
@@ -26,149 +36,75 @@ const TaskForm = (props) => {
         .then(res => res.json())
         .then(data => {
             if (data.error) {
-                setErrorStatus(data.status);
+                props.formError(data.status);
             } else {
-                setSuccess(true);
-                props.handler();
+                props.handlerForm();
             }
         });
+		handleClose();
     };
     const getDone = () => {
         return done === undefined ? (props.done === 'true' ? true : false) : (done === 'true' ? true : false);
     }
+	
+	return (
+		<>
+		<Modal show={show} onHide={handleClose} size='lg' aria-labelledby="task-modal-vcenter" centered>
+			<Modal.Header closeButton>
+			<Modal.Title>Update Task <b>{props.taskID}</b></Modal.Title>
+			</Modal.Header>
+			<Modal.Body>
+				<Form id='TaskForm'>
+					<Form.Row>
+						<Col xs={4}>
+						<Form.Group as={Col} controlId='titleInput'>
+							<Form.Label>Title </Form.Label>
+							<Form.Control 
+								type='text'
+								name='title' 
+								autoComplete='off'
+								defaultValue={props.title}
+								onChange={e => setTitle(e.target.value)}
+							/>
+						</Form.Group>
+						</Col>
+						<Col>
+						<Form.Group as={Col} controlId='textAreaInput'>
+							<Form.Label>Description </Form.Label>
+							<Form.Control as='textarea'
+								name='description'
+								rows='3'
+								defaultValue={props.description}
+								onChange={e => setDescription(e.target.value)}
+							/>
+						</Form.Group>
+						</Col>
+					</Form.Row>
+					<Form.Row>
+					<Col xs={3}>
+						<Form.Group as={Col} controlId='doneSelect'>
+							<Form.Label>Done </Form.Label>
+							<Form.Control as='select' name='done' defaultValue='---' onChange={e => setDone(e.target.value)}>
+								<option>---</option>
+								<option value={true}>Yes</option>
+								<option value={false}>No</option>
+							</Form.Control>
+						</Form.Group>
+						</Col>
+					</Form.Row>
+				</Form>
+			</Modal.Body>
+			<Modal.Footer>
+				<Button variant="secondary" onClick={handleClose}>
+				Close
+				</Button>
+				<Button variant="primary" onClick={handleSubmit}>
+				Save Changes
+				</Button>
+			</Modal.Footer>
+		</Modal>
+		</>
+		);
+}
 
-    return (
-        <>
-        {/* {this.state.success && <Alert variant='success'>Task <b>{this.props.taskID}</b> has been updated!</Alert>} */}
-        <Form onSubmit={handleSubmit} id='TaskForm'>
-            <Form.Row>
-                <Col xs={3}>
-                <Form.Group as={Col} controlId='titleInput'>
-                    <Form.Label>Title </Form.Label>
-                    <Form.Control 
-                        type='text'
-                        name='title' 
-                        autoComplete='off'
-                        defaultValue={props.title}
-                        onChange={e => setTitle(e.target.value)}
-                    />
-                </Form.Group>
-                </Col>
-                <Col xs={1}>
-                <Form.Group as={Col} controlId='doneSelect'>
-                    <Form.Label>Done </Form.Label>
-                    <Form.Control as='select' name='done' defaultValue='---' onChange={e => setDone(e.target.value)}>
-                        <option>---</option>
-                        <option value={true}>Yes</option>
-                        <option value={false}>No</option>
-                    </Form.Control>
-                </Form.Group>
-                </Col>
-                <Col>
-                <Form.Group as={Col} controlId='textAreaInput'>
-                    <Form.Label>Description </Form.Label>
-                    <Form.Control as='textarea'
-                        name='description'
-                        rows='3'
-                        defaultValue={props.description}
-                        onChange={e => setDescription(e.target.value)}
-                    />
-                </Form.Group>
-                </Col>
-            </Form.Row>
-            <Button variant='primary' type='submit'>{props.bText}</Button>
-        </Form>
-        </>
-    )
-} 
-// class TaskForm extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             title: this.props.title,
-//             done: this.props.done,
-//             description: this.props.description,
-//             errorStatus: null,
-//             success: false
-//         }
-//     }
-//     handleChange = (e) => {
-//         let nam = e.target.name;
-//         let val = e.target.value;
-//         this.setState({ [nam]: val })
-//     };
-//     handleSubmit = (e) => {
-//         // e.preventDefault();
-//         fetch(`/api/tasks/${this.props.taskID}`, {
-//             method: this.props.method,
-//             headers: {'Content-Type': 'application/json'},
-//             body: JSON.stringify({
-//                 title: this.state.title,
-//                 done: this.getDone(),
-//                 description: this.state.description
-//             })
-//         })
-//         .then(res => res.json())
-//         .then(data => {
-//             if (data.error) {
-//                 this.setState({errorStatus: data.status});
-//             } else {
-//                 this.setState({success: true});
-//             }
-//         });
-//     };
-//     getDone = () => {
-//         return this.state.done === undefined ? (this.props.done === 'true' ? true : false) : (this.state.done === 'true' ? true : false);
-//     }
-
-//     render() {
-//         // if (this.state.errorStatus) return <Redirect to={`/${this.state.errorStatus}`} />;
-//         // if (this.state.success) return <Redirect to={`/tasks/${this.props.taskID}`}  />;
-
-//         return (
-//             <>
-//             {/* {this.state.success && <Alert variant='success'>Task <b>{this.props.taskID}</b> has been updated!</Alert>} */}
-//             <Form onSubmit={this.handleSubmit} id='TaskForm'>
-//                 <Form.Row>
-//                     <Col xs={3}>
-//                     <Form.Group as={Col} controlId='titleInput'>
-//                         <Form.Label>Title </Form.Label>
-//                         <Form.Control 
-//                             type='text'
-//                             name='title' 
-//                             autoComplete='off'
-//                             defaultValue={this.props.title}
-//                             onChange={this.handleChange}
-//                         />
-//                     </Form.Group>
-//                     </Col>
-//                     <Col xs={1}>
-//                     <Form.Group as={Col} controlId='doneSelect'>
-//                         <Form.Label>Done </Form.Label>
-//                         <Form.Control as='select' name='done' defaultValue={'---'} onChange={this.handleChange}>
-//                             <option>---</option>
-//                             <option value={true}>Yes</option>
-//                             <option value={false}>No</option>
-//                         </Form.Control>
-//                     </Form.Group>
-//                     </Col>
-//                     <Col>
-//                     <Form.Group as={Col} controlId='textAreaInput'>
-//                         <Form.Label>Description </Form.Label>
-//                         <Form.Control as='textarea'
-//                             name='description'
-//                             rows='3'
-//                             defaultValue={this.props.description}
-//                             onChange={this.handleChange}
-//                         />
-//                     </Form.Group>
-//                     </Col>
-//                 </Form.Row>
-//                 <Button variant='primary' type='submit'>{this.props.bText}</Button>
-//             </Form>
-//             </>
-//         )
-//     }
-// }
-
-export default TaskForm;
+export default TaskFormModal;
